525c2e908d40932b44eec89600ae8559d0ae3e66
Richard Russon <rich@flatcap.org>
2020-02-21 01:14:41 +0000

    path: mbox

--- test/main.c
+++ test/main.c
@@ -362,6 +362,14 @@
   NEOMUTT_TEST_ITEM(test_mutt_map_get_name)                                    \
   NEOMUTT_TEST_ITEM(test_mutt_map_get_value)                                   \
                                                                                \
+  /* mbox */                                                                   \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_canon)                                     \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_compare)                                   \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_parent)                                    \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_pretty)                                    \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_probe)                                     \
+  NEOMUTT_TEST_ITEM(test_mbox_path2_tidy)                                      \
+                                                                               \
   /* mbyte */                                                                  \
   NEOMUTT_TEST_ITEM(test_mutt_mb_charlen)                                      \
   NEOMUTT_TEST_ITEM(test_mutt_mb_filter_unprintable)                           \
